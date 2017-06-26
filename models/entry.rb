375219e7d7c43fe1765e3418722e89baa5be73ba
Azucena Martinez <azucena_rys@yahoo.com>
2017-06-26 14:14:15 -0600

    added address book

---
+++ models/entry.rb
@@ -0,0 +1,15 @@
+class Entry
+  attr_accessor :name, :phone_number, :email
+  def initialize(name, phone_number, email)
+    @name = name
+    @phone_number = phone_number
+    @email = email
+  end
+
+  //Here we are creating the string we want
+  def to_s
+     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+   end
+
+end
+#this is our stub which is an empty class.
